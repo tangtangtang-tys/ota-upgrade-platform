f9f49f644d48d237a54307045e594f9363d1a50b
cci <1455604895@qq.com>
2026-01-20 20:49:25 +0800

    Fix base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: process.env.NODE_ENV === 'production' ? '/ota-upgrade-platform/' : '/',
+      base: '/ota-upgrade-platform/',
       server: {
         port: 3000,
         host: '0.0.0.0',
